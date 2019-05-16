ae5f01b5965e68aea0ca9b56edd800c0ca06deb3
odemyanenko <olgadem@hotmail.com>
2019-05-17 00:26:07 +0300

    change gitignore

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const router = express.Router();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+const path = __dirname + '/views/';
+
+const customers = [];
+
+router.use(function (req, res, next) {
+    console.log("/" + req.method);
+    next();
+});
+
+app.get("/", function (req, res) {
+    res.sendFile(path + "index.html");
+});
+
+app.use("/", router);
+
+app.use("*", function (req, res) {
+    res.sendFile(path + "404.html");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
