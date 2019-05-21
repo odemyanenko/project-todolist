251ac791bcf4074f9bee3f65e2e4861d6eb76b47
odemyanenko <olgadem@hotmail.com>
2019-05-21 23:28:22 +0300

    add dbconnect

--- db/config.js
+++ db/config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    //uri: 'mongodb+srv://TODOuser:todouser_fs7@testcluster-8zmxo.mongodb.net/test?retryWrites=true',
-    uri: "mongodb+srv://testUser:7791OlegHome!!@testcluster-8zmxo.mongodb.net/test?retryWrites=true",
-    dbName: "users"//"TODOlist"
+
 }
