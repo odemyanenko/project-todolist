ad2b063b14374336eb8d080b77b1b76903a7c6df
Vova Shkil <vshkil@gmail.com>
2019-05-26 02:50:23 -0400

    adding app.set('port', process.env.PORT || 3000); to app to publish on heroku

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use("/*", pnfRouter);
 
 db.connect();
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+    console.log('Example app listening on port ' + app.get('port'));
 });
